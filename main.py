86e503e1e5901c2585b9310d9f89d6a60bdec440
Anuar <anuar12102001@gmail.com>
2020-08-22 01:56:23 +0600

    first_commit

---
+++ main.py
@@ -0,0 +1,131 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import os
+import csv
+
+
+
+def save_txt(path,content):
+    with open(path,'w',encoding='utf-8')as file:
+        for cont in content:
+            file.write(cont)
+
+def save_csv(file_name,columns,content):
+    with open(file_name,'w',newline='',encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(columns)
+        writer.writerows(content)
+
+
+def files_exist(files):
+    downloads = os.listdir('files/')
+    print(f'downloads {downloads}')
+    print(f'Files: {files}')
+    exists = False
+    for download in downloads:
+        for file in files:
+            if file in download:
+                exists = True
+        if exists:
+            exists = False
+        else:
+            return False
+
+    return True
+    
+
+
+chrome_data_dir = PATH_TO_YOUR_CHROME_ACCOUNT
+URL = 'https://studwork.org/orders'
+PATH = PATH_TO_CHROMEDRIVER
+
+download_directory=os.getcwd()+'\\files\\'
+print(download_directory)
+prefs = {'download.default_directory':download_directory}
+
+options = webdriver.ChromeOptions()
+options.add_argument('user-data-dir='+chrome_data_dir)#set a user
+options.add_experimental_option('prefs',prefs) #change the dowload directory
+
+
+
+driver = webdriver.Chrome(PATH,options=options)
+
+
+if not os.path.exists('orders/'):
+    os.mkdir('orders/')
+
+if not os.path.exists('files/'):
+    os.mkdir('files')
+
+
+parsed_orders = []#container for parsed orders
+cats = ['Название','Раздел','Срок сдачи','Цена','Предмет','Антиплагиат','Тип работы','Описание работы']
+
+orders = driver.find_elements_by_class_name('order-item')
+
+
+for page in range(1,3):
+    driver.get(URL+'?page={}'.format(page))
+    base_window = driver.current_window_handle#current page with orders
+    orders = driver.find_elements_by_class_name('order-item')
+
+
+    for order in orders:
+        link = order.find_element_by_tag_name('a')
+        link.click()
+
+        driver.switch_to.window(driver.window_handles[-1])
+
+        #getting basic categories
+        cells = driver.find_elements_by_class_name('chess-board-cell')
+        card = [driver.title]
+        for cell in cells:
+            cat = cell.text.split('\n')
+            if cat[0] in cats:
+                card.append(cat[-1])
+        
+        parsed_orders.append(card)
+        #get order number
+        order_number = driver.find_element_by_class_name('chess-board-cell__value')
+        print(order_number.text)
+        #getting the description
+        description = driver.find_element_by_xpath('//*[@id="app"]/div[3]/div/div[1]/div[2]/div/div/div[2]/div[2]/div').text
+        parsed_orders[-1].append(description)
+        
+        #getting files
+        file_names = []
+        files = driver.find_elements_by_class_name('file-info.file__info')
+        for file in files:
+            link_text = file.text.split('\n')[0]
+            print(link_text)
+            try:
+                link = driver.find_element_by_link_text(link_text)
+                link.click() #download the file
+                file_names.append(link_text)
+            except:
+                pass
+
+        
+
+        
+        if not os.path.exists('orders\\{}\\'.format(order_number.text)):
+            os.mkdir('orders\\{}\\'.format(order_number.text))
+
+        file_name = 'orders\\{order_number}\\{order_number}.csv'.format(order_number=order_number.text)
+        save_csv(file_name,cats,parsed_orders)
+        save_txt('orders\\{order_number}\\links.txt'.format(order_number=order_number.text),file_names)
+
+        driver.close()#close current window
+        driver.switch_to_window(base_window)#switch to main window(orders page)
+        print(parsed_orders)
+        parsed_orders.clear()
+
+
+
+
+    
+print('Success!')
+driver.quit()
+
